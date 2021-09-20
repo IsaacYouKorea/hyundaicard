7785767ff0380fa48bd1bcdad9b36ccf0bac298e
유영준 <isaac_you@mhand.net>
2021-09-21 03:17:06 +0900

    style: 컬러 스타일 추가

---
+++ src/styles/colors.ts
@@ -0,0 +1,10 @@
+export const BACKGROUND_COLOR="#353336";
+export const TEXT = {
+  RED: "#DE4954",
+  NORMAL: "#959595",
+  ACTIVE: "#FFFFFF",
+  BLUE: "#5DA4E5",
+}
+export const BUTTON = {
+  NORMAL_BG: "#454545",
+}
