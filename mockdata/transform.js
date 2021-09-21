c274e7c03b0b8bb3b580e23fa25b2c8cd6985b00
유영준 <isaac_you@mhand.net>
2021-09-21 15:09:41 +0900

    util: Transform mock data

---
+++ mockdata/transform.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const mockRawData = require('./index.json');
+
+function travel(array) {
+  const result = [];
+  for(const data of array) {
+    result.push(deepen(data));
+  }
+  return JSON.stringify(result);
+}
+
+function deepen(obj) {
+  const result = {};
+
+  // For each object path (property key) in the object
+  for (const objectPath in obj) {
+    // Split path into component parts
+    const parts = objectPath.split('.');
+
+    // Create sub-objects along path as needed
+    let target = result;
+    while (parts.length > 1) {
+      const part = parts.shift();
+      target = target[part] = target[part] || {};
+    }
+
+    // Set value at end of path
+    target[parts[0]] = obj[objectPath]
+  }
+
+  return result;
+}
+
+fs.writeFileSync('mockdata/mock.json', travel(mockRawData));
