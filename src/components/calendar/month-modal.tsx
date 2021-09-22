25e1ccea0aeb132ac5eb4909de6ff4c01e0839b0
유영준 <isaac_you@mhand.net>
2021-09-22 11:16:28 +0900

    feat: 월선택 모달 추가

--- src/components/calendar/month-modal.tsx
+++ src/components/calendar/month-modal.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { usePaymentDispatch, usePaymentState } from "../../context";
 import { monthsBefore } from "../../util/date";
+import Modal from "../Modal";
 
 const StyledMonthModal = styled.div``
 
@@ -9,28 +10,20 @@ const months = monthsBefore();
 function MonthModal() {
   const { date } = usePaymentState();
   const dispatch = usePaymentDispatch();
-  const closeModal = () => undefined;
   const setYearMonth = (year: number, month: number, day: number) => dispatch({ type: 'CALENDAR/SET_DATE', year, month, day })
+  const { modalOpen } = usePaymentState();
 
-  return <StyledMonthModal>
-    <div className="bg">
-      <div className="modal">
-        <div>
-          <div>월선택</div>
-          <button onClick={closeModal}>닫기</button>
-        </div>
-        <div>
-          {months.map((month) =>
-            <button onClick={() =>
-              setYearMonth(month.year, month.month, date.day || 1)
-            }>
-              {month.year}년 {month.month + 1}월
-            </button>
-          )}
-        </div>
-      </div>
+  return <Modal open={modalOpen} title="월선택">
+    <div>
+      {months.map((month) =>
+        <button onClick={() =>
+          setYearMonth(month.year, month.month, date.day || 1)
+        }>
+          {month.year}년 {month.month + 1}월
+        </button>
+      )}
     </div>
-  </StyledMonthModal>
+  </Modal>
 }
 
 export default MonthModal;
--- src/components/calendar/month-select.tsx
+++ src/components/calendar/month-select.tsx
@@ -1,10 +1,14 @@
+import { usePaymentDispatch } from "../../context";
+
 interface IProps {
   year: number
   month: number
 }
 
 function MonthSelect({year, month}: IProps) {
-  return <button>{year}년 {month + 1}월</button>
+  const dispatch = usePaymentDispatch();
+  const openModalMonth = () => dispatch({type: 'UI/OPEN_MODAL', open: true})
+  return <button onClick={openModalMonth}>{year}년 {month + 1}월</button>
 }
 
 export default MonthSelect;
